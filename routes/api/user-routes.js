46519adaa5052145a7548c058117cf3cb82c32cd
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-10-08 16:40:44 -0400

    add routes to get all users and post, get one user, put, and delete, and add and delete a friend

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
 
+// get all users
+// get a single user by its _id and populated thought and friend data
+// post a new user
+// put to update a user by its _id
+// delete to remove user by its _id
+// add and delete a friend
+
 const {
     getAllUsers,
     getUserById,
@@ -9,6 +16,10 @@ const {
     addFriend,
     removeFriend
 } = require("../../controllers/user-controller");
+
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id').get(getUserById).post(updateUser).delete(deleteUser);
+router.route('/:id/friends/:friendId').post(addFriend).delete(removeFriend);
 
 
 
