ef01d72c49cd459f752fcd0cbc254854fe2e7da9
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-10-08 10:31:33 -0400

    update dateformat.js

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -33,3 +33,27 @@ module.exports = (
         11: monthLength === 'short' ? 'Dec' : 'December'
     };
 }
+
+const dateObj = new Date(timestamp);
+const formattedMonth = months[dateObj.getMonth()];
+
+const dayOfMonth = dateSuffix
+? addDateSuffix(dateObj.getDate())
+: dateObj.getDate();
+
+const year = dateObj.getFullYear();
+let hour = dateObj.getHours() > 12
+? Math.floor(dateObj.getHours() - 12)
+: dateObj.getHours();
+
+if (hour === 0) {
+    hour = 12;
+}
+
+const minutes = (dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes();
+
+const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
+
+const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
+
+return formattedTimeStamp;
