14e2504b20a02761cc82a5fb15861ba51231d147
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-10-08 16:32:39 -0400

    update user-routes

--- models/Thought.js
+++ models/Thought.js
@@ -28,3 +28,39 @@ const reactionSchema = new Schema(
         }
     }
 );
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+const thought = model('thought', thoughtSchema);
+
+module.exports = thought;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require("../../controllers/user-controller");
+
+
+
+module.exports = router;
