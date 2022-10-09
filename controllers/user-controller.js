080e2474404060e7d793bc737a873b21400feb85
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-10-09 07:02:51 -0400

    add get all users, getuserbyid, createuser, updateuser to user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,55 @@
+const { user } = require('../models');
+
+// get all Users
+const usersController = {
+    getAllUsers(req, res) {
+        user.find({})
+        .populate({
+            path: 'thoughts',
+            select: '__v'
+        })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    // get user by id
+    getUserById({ params }, res) {
+        user.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    createUser({ body }, res) {
+        user.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    },
+
+    updateUser({ params, body }, res) {
+        user.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    deleteUser({ params }, res) {
+        
+    }
+}
